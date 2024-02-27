5e77778f4fa6d28884d3593fa8824405eb420642
Jonathan Varghese <jonathanvarghese24@gmail.com>
2024-02-27 09:25:21 -0600

    Initial Commit

---
+++ 'Za Hunter/_Za_HunterApp.swift
@@ -0,0 +1,17 @@
+//
+//  _Za_HunterApp.swift
+//  'Za Hunter
+//
+//  Created by Jonathan Varghese on 2/27/24.
+//
+
+import SwiftUI
+
+@main
+struct _Za_HunterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
